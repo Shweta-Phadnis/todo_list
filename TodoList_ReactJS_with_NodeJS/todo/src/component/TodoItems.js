0ec275107618ad5a47c2a91e5e53a831c94f2e0b
Shweta-Phadnis <91286504+Shweta-Phadnis@users.noreply.github.com>
2023-01-19 21:55:38 +0530

    Update TodoItems.js

--- TodoList_ReactJS_with_NodeJS/todo/src/component/TodoItems.js
+++ TodoList_ReactJS_with_NodeJS/todo/src/component/TodoItems.js
@@ -2,38 +2,21 @@ import React, { useState } from "react";
 import DeleteIcon from '@mui/icons-material/Delete';
 import CheckIcon from '@mui/icons-material/Check';
 
-const styles = {
-    title : {
-        color : 'rgb(15 20 25)',
-        fontSize : '20px',
-        marginLeft : "20px"
-
-    },
-    container : {
-        paddingTop : '20px',
-        height : '50px',
-    }
-}
-
 const TodoItems = (props) => {
-    debugger
     let {name, activeStatus, deActiveStatus, completed, id} = props.item
     const deleteItems = props.deleteItems
     const setStatus = props.setStatus
     const taskCompleted = props.taskCompleted
-    let isChecked = false
     const decideStatus = (value) => {
-        debugger
-        isChecked = value == true? true : false
         value == true? setStatus(id, 'active') : setStatus(id, 'deactive')
     }
 
 
     return(
         <div>
-            <li class="list-group-item" style={{display: 'flex',justifyContent: 'space-between'}}>
+            <li key={id} className="list-group-item" style={{display: 'flex',justifyContent: 'space-between'}}>
             <label>
-                <input type="checkbox"
+                <input type="checkbox" key={`key_${id}`}
                        checked={activeStatus ? 'checked' : ''}
                        onChange={(event) => decideStatus(event. target. checked)}
                 />
