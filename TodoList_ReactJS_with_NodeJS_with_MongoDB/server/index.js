cd24e22adeb59228ab38261f30f7411186dc495b
Shweta-Phadnis <91286504+Shweta-Phadnis@users.noreply.github.com>
2023-01-23 14:10:31 +0530

    Add files via upload

---
+++ TodoList_ReactJS_with_NodeJS_with_MongoDB/server/index.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+
+const cors = require("cors");
+app.use(cors());
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+const mongoURL = 'mongodb://localhost:27017/todoList'
+mongoose.set('strictQuery', true);
+mongoose.connect(mongoURL, (err)=>{
+  if(err){
+    console.log("failed collection")
+  }else{
+    console.log("mongo is connected")
+  }
+})
+
+app.get("/", (req, res) => {
+  res.status(200).json({ status: "success", message: "Welcome from NodeJs" });
+});
+
+
+const taskRouter = require('./router/tasks')
+app.use('/tasks', taskRouter)
+
+app.use((req, res) => {
+  res.status(404).json({ status: "error", message: "404 Not Found" });
+});
+
+app.listen(8000, () => {
+  console.log("Server started on port 8000");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const routes = require('./controller/tasksOperations');
+// app.get("/tasks", routes.getTasks);
+// app.post("/tasks", routes.addTask);
+// app.put("/tasks/completed/:id", routes.updateTask);
+// app.put("/tasks/status/:id", routes.updateTaskStatus);
+// app.delete("/tasks/:id", routes.deleteTask);
